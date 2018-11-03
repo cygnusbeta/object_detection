61363f15ea95b842a8efcb65ae2af3cae035d48c
cygnus_beta <betacygnib+git@gmail.com>
2018-11-04 01:07:12 +0900

    Add files

---
+++ Readme.md
@@ -0,0 +1,59 @@
+## 1.Capture Image
+
+`python capture_img.py`
+
+Prerequisite:
+- opencv-python
+- web camera
+
+## 2.Labeling
+
+`python labeling.py`
+
+Prerequisite:
+- opencv-python
+
+## 3.Create Datasets
+
+`python create_tf_record`
+
+## 4.Train
+
+```
+python object_detection/train.py 
+  --pipeline_config_path=../../data/ssd_mobilenet_v1.config 
+  --train_dir=../../data/train 
+  --logtostderr
+```
+
+## 5.Eval
+
+```
+python object_detection/eval.py
+  --pipeline_config_path=../../data/ssd_mobilenet_v1.config
+  --checkpoint_dir=../../data/train
+  --eval_dir=../../data/train
+  --logtostderr
+```
+
+## 6.Tensorboard
+
+`tensorboard --logdir=train`
+
+## 7.Freeze Model
+
+```
+python object_detection/export_inference_graph.py
+  --input_type image_tensor
+  --pipeline_config_path ../../data/ssd_mobilenet_v1.config
+  --trained_checkpoint_prefix ../../data/train/model.ckpt
+  --output_directory ../../data/save
+```
+
+## 8.Use Model
+
+`python detect.py`
+
+---
+
+Contact: onlyzs1023@gmail.com
---
+++ pre/capture_img.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import argparse,os,sys
+
+IMAGE_DIR = "../data/images"
+NAME = "Popuko" # ラベル名
+
+inWidth = 600
+inHeight = 400
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--video", help="Video File") # ビデオから写真をキャプチャする場合はパラメータを追加
+  args = parser.parse_args()
+  
+  if args.video is not None:
+    cap = cv2.VideoCapture(args.video)
+  else:
+    cap = cv2.VideoCapture(0)
+  
+  #fps = cap.get(cv2.CAP_PROP_FPS)
+  
+  # make dir if not existed
+  if not os.path.exists(IMAGE_DIR):
+    os.makedirs(IMAGE_DIR)
+  
+  counter = 1
+  while True:
+    _, frame = cap.read()
+    if frame is not None:
+      frame = cv2.resize(frame,(inWidth,inHeight))
+      cv2.imshow("capture", frame)
+      if cv2.waitKey(1) & 0xFF == ord("c"):
+        img_name = os.path.join(IMAGE_DIR,NAME+"_"+str(counter)+".jpg")
+        cv2.imwrite(img_name, frame)
+        print("Captured: "+img_name)
+        counter+=1
+      if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    else:
+      sys.exit(0)
+    
+    
